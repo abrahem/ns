c15d9f23dd45df1e9ef741f4d9cca1f5e1bfeee1
abrahem <abrheem16@gmail.com>
2020-02-19 21:22:11 +0300

    Update index.vue

--- src/pages/Basic/index.vue
+++ src/pages/Basic/index.vue
@@ -2,7 +2,6 @@
   <q-page padding>
     <editor-content class="editor__content" :editor="myeditor" />
     <div class="q-pa-md">
-      
   </div>
   </q-page>
 </template>
@@ -10,24 +9,83 @@
 <script>
 import { UiMixin, PageMixin } from 'src/mixins'
 import Toolbar from './toolbar'
+import { Editor, EditorContent } from 'tiptap'
+import {
+  Blockquote,
+  CodeBlock,
+  HardBreak,
+  Heading,
+  HorizontalRule,
+  OrderedList,
+  BulletList,
+  ListItem,
+  TodoItem,
+  TodoList,
+  Bold,
+  Code,
+  Italic,
+  Link,
+  Strike,
+  Underline,
+  History
+} from 'tiptap-extensions'
+
 export default {
   mixins: [UiMixin, PageMixin],
   components: {
+    EditorContent
   },
   data () {
     return {
+      myeditor: new Editor({
+        extensions: [
+          new Blockquote(),
+          new BulletList(),
+          new CodeBlock(),
+          new HardBreak(),
+          new Heading({ levels: [1, 2, 3] }),
+          new HorizontalRule(),
+          new ListItem(),
+          new OrderedList(),
+          new TodoItem(),
+          new TodoList(),
+          new Bold(),
+          new Code(),
+          new Italic(),
+          new Link(),
+          new Strike(),
+          new Underline(),
+          new History()
+        ]
+      })
     }
   },
   beforeDestroy () {
+    this.myeditor.destroy()
     this.$q.loading.hide()
   },
   methods: {
     onSwitch () {
+      this.setEditor(this.myeditor)
       this.setToolbar(Toolbar)
     },
     showLoading () {
       this.$q.loading.show({
         message: '<span>يرجى ألانتظار</span>'
+      })
+    },
+    loaddata () {
+      this.$axios.get('/api/backend')
+      .then((response) => {
+        this.data = response.data
+      })
+      .catch(() => {
+        this.$q.notify({
+          color: 'negative',
+          position: 'top',
+          message: 'Loading failed',
+          icon: 'report_problem'
+        })
       })
     }
   }
