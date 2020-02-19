e36309cbab2a69a77f4d4356a5e4a61f00c64e01
abrahem <abrheem16@gmail.com>
2020-02-19 16:24:21 +0300

    Update

--- src/components/LeftDrawer.vue
+++ src/components/LeftDrawer.vue
@@ -6,134 +6,40 @@
   >
     <q-list>
       <q-item to="/" exact>
-        <q-item-section avatar>
-          <q-icon name="school" />
-        </q-item-section>
         <q-item-section>
-          <q-item-label>Test</q-item-label>
+          <q-item-label>الرئيسية</q-item-label>
         </q-item-section>
       </q-item>
       <q-item to="/menu-bubble">
-        <q-item-section avatar>
-          <q-icon name="chat" />
-        </q-item-section>
         <q-item-section>
-          <q-item-label>Menu Bubble</q-item-label>
+          <q-item-label>قائمة ألانمي</q-item-label>
         </q-item-section>
       </q-item>
       <q-item to="/floating-menu">
-        <q-item-section avatar>
-          <q-icon name="more" />
-        </q-item-section>
         <q-item-section>
-          <q-item-label>Floating Menu</q-item-label>
+          <q-item-label>حسب الموسم</q-item-label>
         </q-item-section>
       </q-item>
       <q-item to="/links">
-        <q-item-section avatar>
-          <q-icon name="link" />
-        </q-item-section>
         <q-item-section>
-          <q-item-label>Links</q-item-label>
+          <q-item-label>حسب التصنيف</q-item-label>
         </q-item-section>
       </q-item>
       <q-item to="/images">
-        <q-item-section avatar>
-          <q-icon name="photo" />
-        </q-item-section>
         <q-item-section>
-          <q-item-label>Images</q-item-label>
+          <q-item-label>أخبار ألانمي</q-item-label>
         </q-item-section>
       </q-item>
       <q-item to="/hiding-menubar">
-        <q-item-section avatar>
-          <q-icon name="keyboard_hide" />
-        </q-item-section>
         <q-item-section>
-          <q-item-label>Hiding Menu Bar</q-item-label>
+          <q-item-label>المواعيد</q-item-label>
         </q-item-section>
       </q-item>
       <q-item to="/todo-list">
-        <q-item-section avatar>
-          <q-icon name="check_box" />
-        </q-item-section>
         <q-item-section>
-          <q-item-label>Todo List</q-item-label>
+          <q-item-label>أنمياتي</q-item-label>
         </q-item-section>
       </q-item>
-      <q-item to="/tables">
-        <q-item-section avatar>
-          <q-icon name="grid_on" />
-        </q-item-section>
-        <q-item-section>
-          <q-item-label>Tables</q-item-label>
-        </q-item-section>
-      </q-item>
-      <q-item to="/suggestions">
-        <q-item-section avatar>
-          <q-icon name="alternate_email" />
-        </q-item-section>
-        <q-item-section>
-          <q-item-label>Suggestions</q-item-label>
-        </q-item-section>
-      </q-item>
-      <q-item to="/markdown-shortcuts">
-        <q-item-section avatar>
-          <q-icon name="title" />
-        </q-item-section>
-        <q-item-section>
-          <q-item-label>Markdown Shortcuts</q-item-label>
-        </q-item-section>
-      </q-item>
-      <q-item to="/code-highlighting">
-        <q-item-section avatar>
-          <q-icon name="code" />
-        </q-item-section>
-        <q-item-section>
-          <q-item-label>Code Highlighting</q-item-label>
-        </q-item-section>
-      </q-item>
-      <q-item to="/history">
-        <q-item-section avatar>
-          <q-icon name="history" />
-        </q-item-section>
-        <q-item-section>
-          <q-item-label>History</q-item-label>
-        </q-item-section>
-      </q-item>
-      <q-item to="/read-only">
-        <q-item-section avatar>
-          <q-icon name="remove_red_eye" />
-        </q-item-section>
-        <q-item-section>
-          <q-item-label>Read-Only</q-item-label>
-        </q-item-section>
-      </q-item>
-      <q-item to="/embeds">
-        <q-item-section avatar>
-          <q-icon name="picture_in_picture" />
-        </q-item-section>
-        <q-item-section>
-          <q-item-label>Embeds</q-item-label>
-        </q-item-section>
-      </q-item>
-      <q-item to="/placeholder">
-        <q-item-section avatar>
-          <q-icon name="aspect_ratio" />
-        </q-item-section>
-        <q-item-section>
-          <q-item-label>Placeholder</q-item-label>
-        </q-item-section>
-      </q-item>
-      <q-item to="/export">
-        <q-item-section avatar>
-          <q-icon name="import_export" />
-        </q-item-section>
-        <q-item-section>
-          <q-item-label>Export HTML or JSON</q-item-label>
-        </q-item-section>
-      </q-item>
-
     </q-list>
   </q-drawer>
 </template>
