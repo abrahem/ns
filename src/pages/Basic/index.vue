dac5d48cafe0cafbb15fa8084bd048fc3db6116f
abrahem <abrheem16@gmail.com>
2020-02-20 16:32:27 +0300

    Update index.vue

--- src/pages/Basic/index.vue
+++ src/pages/Basic/index.vue
@@ -93,9 +93,11 @@ export default {
       this.setToolbar(Toolbar)
     },
     showLoading () {
-      this.$q.loading.show({
-        message: '<span>يرجى ألانتظار</span>'
-      })
+this.$request('http://www.google.com', function (error, response, body) {
+  console.error('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
     }
   }
 }
