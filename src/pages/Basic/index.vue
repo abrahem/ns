b6a343fd556d4dab9b5f174c27002a27a5b01689
abrahem <abrheem16@gmail.com>
2020-02-20 22:48:58 +0300

    Update index.vue

--- src/pages/Basic/index.vue
+++ src/pages/Basic/index.vue
@@ -1,6 +1,5 @@
 <template>
   <q-page padding>
-    <editor-content class='editor__content' :editor='myeditor' />
         <q-list bordered>
       <q-item clickable v-ripple>
         <q-item-section>List item</q-item-section>
@@ -117,55 +116,15 @@
 <script>
 import { UiMixin, PageMixin } from 'src/mixins'
 import Toolbar from './toolbar'
-import { Editor, EditorContent } from 'tiptap'
-import {
-  Blockquote,
-  CodeBlock,
-  HardBreak,
-  Heading,
-  HorizontalRule,
-  OrderedList,
-  BulletList,
-  ListItem,
-  TodoItem,
-  TodoList,
-  Bold,
-  Code,
-  Italic,
-  Link,
-  Strike,
-  Underline,
-  History
-} from 'tiptap-extensions'
 
 export default {
   mixins: [UiMixin, PageMixin],
   components: {
-    EditorContent
+
   },
   data () {
     return {
-      myeditor: new Editor({
-        extensions: [
-          new Blockquote(),
-          new BulletList(),
-          new CodeBlock(),
-          new HardBreak(),
-          new Heading({ levels: [1, 2, 3] }),
-          new HorizontalRule(),
-          new ListItem(),
-          new OrderedList(),
-          new TodoItem(),
-          new TodoList(),
-          new Bold(),
-          new Code(),
-          new Italic(),
-          new Link(),
-          new Strike(),
-          new Underline(),
-          new History()
-        ]
-      })
+
     }
   },
   beforeDestroy () {
@@ -187,13 +146,10 @@ export default {
             icon: 'report_problem'
           })
         })
-    },
-    onSwitch () {
-      this.setEditor(this.myeditor)
-      this.setToolbar(Toolbar)
     }
   },
   created () {
+    this.setToolbar(Toolbar)
     this.loadData()
   }
 }
