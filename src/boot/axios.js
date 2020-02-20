bb7ce2ddf01d71dd686e9e48126327d07a967244
abrahem <abrheem16@gmail.com>
2020-02-20 20:06:47 +0300

    Update axios.js

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const axiosInstance = axios.create({
-    baseURL:'https://api.example.com'
+    baseURL: 'https://api.example.com'
 })
 export default ({ Vue }) => {
     Vue.prototype.$axios = axios
