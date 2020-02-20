bd3ff97d74aa510b961079e561b31b4c6ee39685
abrahem <abrheem16@gmail.com>
2020-02-20 21:15:31 +0300

    Update index.vue

--- src/pages/Basic/index.vue
+++ src/pages/Basic/index.vue
@@ -92,19 +92,7 @@ export default {
       this.setToolbar(Toolbar);
     },
     loaddata() {
-      this.$axios
-      .get("/")
-      .then(response => {
-        this.data = response.data;
-        })
-        .catch(() => {
-          this.$q.notify({
-            color: "negative",
-            position: "top",
-            message: "Loading failed",
-            icon: "report_problem"
-            });
-        });
+      
     }
   }
 };
