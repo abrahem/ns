ed069643e1f44ceaaa1f6a66e3e921cde71bfe75
abrahem <abrheem16@gmail.com>
2020-02-20 22:41:38 +0300

    Update index.vue

--- src/pages/Basic/index.vue
+++ src/pages/Basic/index.vue
@@ -165,28 +165,7 @@ export default {
           new Underline(),
           new History()
         ],
-        content: `
-          <h2>
-            Hi there,
-          </h2>
-          <p>
-            this is a very <em>basic</em> example of tiptap.
-          </p>
-          <pre><code>body { display: none }</code></pre>
-          <ul>
-            <li>
-              A regular list
-            </li>
-            <li>
-              With regular items
-            </li>
-          </ul>
-          <blockquote>
-            It's amazing 👏
-            <br />
-            – mom
-          </blockquote>
-        `
+        content: ``
       })
     }
   },
