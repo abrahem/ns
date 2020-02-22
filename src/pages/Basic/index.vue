6e5f9bf06729bef5a33d3c84594cf69da7486d69
abrahem <abrheem16@gmail.com>
2020-02-22 17:31:37 +0300

    Update index.vue

--- src/pages/Basic/index.vue
+++ src/pages/Basic/index.vue
@@ -2,7 +2,10 @@
   <q-page padding>
         <q-list bordered>
       <q-item v-for="item in items" v-bind:key="item.id" style="padding: 0 !important; border-bottom: inherit;" clickable v-ripple>
-        <q-item-section style="text-align: right; margin-bottom: auto; padding-top: 5px;">{{ item.name }}</q-item-section>
+        <q-item-section>
+          <q-item-label>{{ item.name }}</q-item-label>
+          <q-item-label caption>{{ item.id }}</q-item-label>
+        </q-item-section>
         <q-item-section style="margin-left: -11px !important; margin-right: 0px !important;" thumbnail>
           <img style="height: 125px; width: 90px;" src={{ item.image }}>
         </q-item-section>
