2895c8b06313fd55fd528b0535f5bf901f2bc26e
abrahem <abrheem16@gmail.com>
2020-02-22 17:24:20 +0300

    Update index.vue

--- src/pages/Basic/index.vue
+++ src/pages/Basic/index.vue
@@ -4,7 +4,7 @@
       <q-item v-for="item in items" v-bind:key="item.id" style="padding: 0 !important; border-bottom: inherit;" clickable v-ripple>
         <q-item-section style="text-align: right; margin-bottom: auto; padding-top: 5px;">{{ item.name }}</q-item-section>
         <q-item-section style="margin-left: -11px !important; margin-right: 0px !important;" thumbnail>
-          <img style="height: 125px; width: 90px;" src="https://cdn.quasar.dev/img/mountains.jpg">
+          <img style="height: 125px; width: 90px;" src="{{ item.image }}">
         </q-item-section>
       </q-item>
     </q-list>
@@ -32,9 +32,13 @@ export default {
   },
   methods: {
     loadData () {
+      this.$q.loading.show({
+        message: '<span>يرجى الأنتظار</span>'
+      })
       this.$axios
         .get('https://snoanime.com/ns/api/new/')
         .then(response => {
+          this.$q.loading.hide()
           var self = this
           this.data = response.data
           self.items = response.data
