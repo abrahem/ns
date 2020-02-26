898b51753dd2434575d23fe716421a655a56d256
abrahem <abrheem16@gmail.com>
2020-02-26 19:40:07 +0300

    Update index.vue

--- src/pages/Basic/index.vue
+++ src/pages/Basic/index.vue
@@ -38,8 +38,33 @@
     <q-page-container>
         <q-tab-panels v-model="tab" animated class="text-black">
           <q-tab-panel name="info">
-            <div class="text-h6">Mails</div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    <q-card class="my-card">
+      <q-card-section horizontal>
+        <q-img
+          class="col-5"
+          src="https://cdn.quasar.dev/img/parallax1.jpg"
+        />
+
+        <q-card-section>
+          "testkasdlasdlaskldaskdklaskdasldasldasldaskdkasdadadadas"
+        </q-card-section>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions>
+        <q-btn flat round icon="event" />
+        <q-btn flat>
+          5:30PM
+        </q-btn>
+        <q-btn flat>
+          7:00PM
+        </q-btn>
+        <q-btn flat color="primary">
+          Reserve
+        </q-btn>
+      </q-card-actions>
+    </q-card>
           </q-tab-panel>
 
           <q-tab-panel name="ep">
@@ -115,3 +140,8 @@ export default {
   }
 }
 </script>
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 350px
+</style>
